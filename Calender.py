a8759f833ca246b04dd36554f8d0803bd3438c74
samsonpaul <samsonpaul4@gmail.com>
2016-11-16 14:20:34 +0300

    Implementation of the AddEvent Method

--- Calender.py
+++ Calender.py
@@ -5,12 +5,16 @@ class Calender(object):
 		Calender App to keep track of events on their calendar.
 	"""
 	def __init__(self, name):
-		self.name = 
+		self.name = name
 		self.event_dict = {}
 
-	def addEvent():
+	def addEvent(self):
 		#Function to add events to the calender
-		pass
+		date =  input(Enter Date of the Event)
+		name =  input(Enter Name of the Event)
+		time =  input(Set Time of the Event)
+		date_time = (date, time)
+		event_dict[date_time] = name
 
 	def view_LastEvent():
 		#Function to view LastEvent on the calender
