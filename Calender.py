f22d014d6de6cc26139874ae140852fc6ad5084d
Jimnah Magira <Jimnah Magira>
2016-11-16 13:54:42 +0300

    Adds Logic

--- Calender.py
+++ Calender.py
@@ -1,9 +1,12 @@
+import sys
+
 class Calender(object):
 	"""
 		Calender App to keep track of events on their calendar.
 	"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self, name):
+		self.name = 
+		self.event_dict = {}
 
 	def addEvent():
 		#Function to add events to the calender
@@ -16,3 +19,13 @@ class Calender(object):
 	def list_AllEvents():
 		#Function to view all events on the calender
 		pass
+
+
+if __name__ == "__main__":
+	user = sys.argv[1]
+    method = sys.argv[2]
+    user = Calender(user)
+    method = method.upper()
+    if method in [ADD, LIST_LAST, LIST_ALL]:
+    	pass
+    
