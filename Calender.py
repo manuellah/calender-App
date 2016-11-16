d4cefb60d502972737fb132b4980c687a3c537fc
Jimnah Magira <Jimnah Magira>
2016-11-16 14:34:49 +0300

    Adds New_Command function

--- Calender.py
+++ Calender.py
@@ -5,27 +5,40 @@ class Calender(object):
 		Calender App to keep track of events on their calendar.
 	"""
 	def __init__(self, name):
-		self.name = 
+		self.name = name
 		self.event_dict = {}
 
-	def addEvent():
+	def addEvent(self):
 		#Function to add events to the calender
 		pass
 
-	def view_LastEvent():
+	def view_LastEvent(self):
 		#Function to view LastEvent on the calender
 		pass
 		
-	def list_AllEvents():
+	def list_AllEvents(self):
 		#Function to view all events on the calender
 		pass
+	def new_command(self):
+		cmd = input('Enter a command')
+		cmd = cmd.upper()
+
+		if cmd in ['ADD', 'LIST_LAST', 'LIST_ALL', 'N']:
+			if method == 'ADD':
+				user.addEvent()
+			elif method == 'LIST_ALL':
+				user.list_AllEvents()
+			elif method == 'LIST_LAST':
+				user.view_LastEvent()
+			elif method == 'N':
+				sys.exit()
+
+		else:
+			print 'Invalid command ', cmd
+
+
+
 
 
 if __name__ == "__main__":
-	user = sys.argv[1]
-    method = sys.argv[2]
-    user = Calender(user)
-    method = method.upper()
-    if method in [ADD, LIST_LAST, LIST_ALL]:
-    	pass
-    
+	Calender(sys.argv[1]).new_command()
