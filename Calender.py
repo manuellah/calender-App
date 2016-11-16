71f6b10373e2bbd92e8583d47287a9d8ff4b291f
samsonpaul <samsonpaul4@gmail.com>
2016-11-16 13:02:49 +0300

    Addition of the Skeleton Base of the Calender App

---
+++ Calender.py
@@ -0,0 +1,18 @@
+class Calender(object):
+	"""
+		Calender Object
+	"""
+	def __init__(self, arg):
+		self.arg = arg
+
+	def addEvent():
+		#Function to add events to the calender
+		pass
+
+	def view_LastEvent():
+		#Function to view LastEvent on the calender
+		pass
+		
+	def list_AllEvents():
+		#Function to view all events on the calender
+		pass
